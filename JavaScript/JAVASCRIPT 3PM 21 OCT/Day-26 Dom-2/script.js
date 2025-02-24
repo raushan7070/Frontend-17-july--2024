a0b818dfe64586507e40b6b81bd7ddc6ec527ba5
Raushan Kumar <85994243+raushan7070@users.noreply.github.com>
2025-02-24 20:05:41 +0530

    Add files via upload

---
+++ JavaScript/JAVASCRIPT 3PM 21 OCT/Day-26 Dom-2/script.js
@@ -0,0 +1,24 @@
+const hero=document.getElementById("hero");
+const myImage="https://m.media-amazon.com/images/M/MV5BMWI4N2Y5NWUtNzEwOC00YjYzLWEzY2ItN2YwYTIxYzBjZGZmXkEyXkFqcGc@._V1_.jpg"
+hero.innerHTML= `<div class="card">
+                 <div class="profile">
+                 <img src=${myImage}>
+                 </div>
+                 <div class="btns">
+                 <a href="">Book Now</a>
+                 <a href="">Read more</a>
+                 </div>
+                 </div>`
+
+                //  const hero= document.getElementById("hero")
+
+                //  console.log(hero.innerText)   // visibile text
+                //  console.log(hero.textContent)   // visbile text hiddent text
+                //  console.log(hero.innerHTML)     //  <span>Visible text</span> <p>Hiddent Text</p>
+                //  console.log(hero.outerHTML)
+
+//  <section id="hero">
+//         <span>Visible Content</span>
+//         <p style="display: none;">Hiddent text</p>
+// </section>
+ 
