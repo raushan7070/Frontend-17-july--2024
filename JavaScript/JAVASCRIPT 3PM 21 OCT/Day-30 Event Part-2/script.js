74738dae12b1cc151947c30d0fdf1b2b8f4a5fdf
Raushan Kumar <85994243+raushan7070@users.noreply.github.com>
2025-04-05 01:11:15 +0530

    new commit

---
+++ JavaScript/JAVASCRIPT 3PM 21 OCT/Day-30 Event Part-2/script.js
@@ -0,0 +1,18 @@
+const btn= document.querySelector("button");
+
+/**
+btn.addEventListener("click",function(){
+    const body= document.body;
+    body.style.cssText="background-color:black;color:white;";
+})
+
+ */
+function darkMode(){
+    const body= document.body;
+    body.style.cssText="background-color:black;color:white;";
+}
+
+
+btn.addEventListener("click",darkMode)
+btn.removeEventListener("click",darkMode)
+
